9914adea810589bc7385aee6a189fdc6c9b8673d
Chethan B <chethan25.bpac@gmail.com>
2021-11-27 18:02:47 +0530

    built popular-categories component with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './styles.css';
 import Header from './components/Header.js';
+import PopularCategories from './components/PopularCategories';
 
 function App() {
   return (
     <>
       <Header />
+      <PopularCategories />
     </>
   );
 }
---
+++ src/components/PopularCategories.js
@@ -0,0 +1,103 @@
+import BreakfastIcon from '../assets/icons/breakfast.svg';
+import VegetarianIcon from '../assets/icons/vegetarian.svg';
+import SeafoodIcon from '../assets/icons/seafood.svg';
+import StarterIcon from '../assets/icons/starter.png';
+import ChickenIcon from '../assets/icons/chicken.svg';
+import DessertIcon from '../assets/icons/dessert.png';
+import PastaIcon from '../assets/icons/pasta.png';
+import VeganIcon from '../assets/icons/vegan.png';
+import './popular-categories.css';
+
+const PopularCategories = () => {
+  return (
+    <>
+      <section>
+        <h2 className="section-title">Popular Categories</h2>
+        <div className="categories">
+          <div className="category breakfast">
+            <img
+              className="categories-icon"
+              src={BreakfastIcon}
+              alt="breakfast"
+            ></img>
+            <h4 className="categoreis-title">Breakfast</h4>
+            <div>
+              <p className="categories-count">7 Recipes</p>
+            </div>
+          </div>
+          <div className="category vegetarian">
+            <img
+              className="categories-icon"
+              src={VegetarianIcon}
+              alt="vegetarian"
+            ></img>
+            <h4 className="categoreis-title">Vegetarian</h4>
+            <div>
+              <p className="categories-count">32 Recipes</p>
+            </div>
+          </div>
+          <div className="category seafood">
+            <img
+              className="categories-icon"
+              src={SeafoodIcon}
+              alt="seafood"
+            ></img>
+            <h4 className="categoreis-title">Seafood</h4>
+            <div>
+              <p className="categories-count">27 Recipes</p>
+            </div>
+          </div>
+          <div className="category starter">
+            <img
+              className="categories-icon"
+              src={StarterIcon}
+              alt="starter"
+            ></img>
+            <h4 className="categoreis-title">Starter</h4>
+            <div>
+              <p className="categories-count">4 Recipes</p>
+            </div>
+          </div>
+          <div className="category chicken">
+            <img
+              className="categories-icon"
+              src={ChickenIcon}
+              alt="chicken"
+            ></img>
+            <h4 className="categoreis-title">Chicken</h4>
+            <div>
+              <p className="categories-count">35 Recipes</p>
+            </div>
+          </div>
+          <div className="category dessert">
+            <img
+              className="categories-icon"
+              src={DessertIcon}
+              alt="dessert"
+            ></img>
+            <h4 className="categoreis-title">Dessert</h4>
+            <div>
+              <p className="categories-count">64 Recipes</p>
+            </div>
+          </div>
+          <div className="category pasta">
+            <img className="categories-icon" src={PastaIcon} alt="pasta"></img>
+            <h4 className="categoreis-title">Pasta</h4>
+            <div>
+              <p className="categories-count">8 Recipes</p>
+            </div>
+          </div>
+          <div className="category vegan">
+            <img className="categories-icon" src={VeganIcon} alt="vegan"></img>
+            <h4 className="categoreis-title">Vegan</h4>
+            <div>
+              <p className="categories-count">3 Recipes</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default PopularCategories;
